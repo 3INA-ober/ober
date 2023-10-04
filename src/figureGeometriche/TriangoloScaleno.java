c6f34748aeffdb58982a0228a7e9add79092f348
david.ober <david.ober@AULAS51-PC19.itib.local>
2023-10-04 09:47:28 +0200

    correzione

--- src/figureGeometriche/TriangoloScaleno.java
+++ src/figureGeometriche/TriangoloScaleno.java
@@ -6,20 +6,26 @@ public class TriangoloScaleno {
    public float lato1;
    public float lato2;
    public float lato3;
+   public float altezza;
     
-    public TriangoloScaleno (float lato1,float lato2,float lato3){
-            this.lato1 = lato1;
+    public TriangoloScaleno (float lato2,float lato3,float altezza, float base){
+            lato1 = base;
             this.lato2 = lato2;
             this.lato3 = lato3;
+            this.altezza = altezza;
            
     }
     
-    public float CalcolaPerimetro (){
+    public float calcolaPerimetro (){
         float perimetro ;
         perimetro = lato1 + lato2 + lato3 ;
         return perimetro;
 }
-    public float CalcolaArea (int perimetro){
-        
-    }   
+    public float calcolaArea (){
+       float area;
+       area = (lato1 * altezza)/2;
+       return area;
     
+    }
+}
+
