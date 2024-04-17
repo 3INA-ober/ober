fe441a1503d3fc003ac951198d12681312961e48
david.ober <david.ober@AULAS51-PC24.itib.local>
2024-04-17 10:15:35 +0200

    classe mcm

---
+++ src/Array/ConvBinDec.java
@@ -0,0 +1,21 @@
+
+package Array;
+
+
+public class ConvBinDec {
+  private String numBin;
+
+    public ConvBinDec(String numBin) {
+        this.numBin = numBin;
+    }
+
+    public String getNumBin() {
+        if (numBin != null){
+            
+        }
+        return numBin;
+    }
+  
+  
+   
+}
